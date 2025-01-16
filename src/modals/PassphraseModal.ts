36ddcd30ae7470b077bb6ff99a13315acd7989bb
Tjado Mäcke <git@tja.do>
2025-01-16 20:43:33 +0100

    Fix note-overwriting bug

--- src/modals/PassphraseModal.ts
+++ src/modals/PassphraseModal.ts
@@ -1,4 +1,4 @@
-import { App, Modal } from "obsidian";
+import { App, Modal, MarkdownView } from "obsidian";
 
 export default class PassphraseModal extends Modal {
 	private passphraseInput: HTMLInputElement;
@@ -13,6 +13,15 @@ export default class PassphraseModal extends Modal {
     
 	openAndAwait(additionalDescription?: string){
 		this.additionalDescription = additionalDescription;
+
+		// Obsidian has a short state-change delay when switching from one note
+		// to another. In that time it is possible to edit one note that is getting
+		// written to the other one. To avoid it, the following code removes the
+		// focus of the active view in case the passphrase modal gets opened.
+		const mdView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (mdView) {
+			mdView.editor.blur();
+		}
 
 		return new Promise<string>((resolve, reject) => {
 			this.resolve = resolve;
@@ -55,8 +64,12 @@ export default class PassphraseModal extends Modal {
 		};
 
 		this.passphraseInput.addEventListener("keydown", (event) => {
+			console.log("press enter");
 			if (event.key === "Enter" && this.passphraseInput.value) {
 				submitButton.click();
+				
+				event.preventDefault();
+				event.stopPropagation();
 			}
 		});
 	}
