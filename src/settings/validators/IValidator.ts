ce482ff8ab8674f876e132b1ffab98aa791337fd
Kevin <identityofsine@gmail.com>
2025-05-29 18:59:35 -0400

    adding validator interface

---
+++ src/settings/validators/IValidator.ts
@@ -0,0 +1,12 @@
+
+export class ValidationError extends Error {
+
+	constructor(public readonly objectName: string, public readonly displayError: string, error: string) {
+		super(error)
+	}
+
+}
+
+export interface IValidator<T> {
+	validate(object: T): ValidationError | undefined
+} 
