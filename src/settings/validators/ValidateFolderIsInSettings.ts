700777d8a7b25f8479db5da06606f729a80bcd24
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2025-12-14 21:23:22 +0100

    Replace NodeJS path by normalizePath (#53)
    Co-authored-by: tjado mäcke <git@tja.do>

--- src/settings/validators/ValidateFolderIsInSettings.ts
+++ src/settings/validators/ValidateFolderIsInSettings.ts
@@ -1,33 +1,40 @@
 import { IValidator, ValidationError } from "./IValidator";
 import { Settings } from "../Settings";
-import * as path from "path";
+import { normalizePath } from "obsidian";
 
 export class FolderInSettingValidator implements IValidator<string> {
-
-	constructor(private readonly settings: Settings) { }
+	constructor(private readonly settings: Settings) {}
 
 	validate(filePath: string) {
-		const normalizedFilePath = path.resolve(filePath);
+		const normalizedFilePath = normalizePath(filePath);
 
 		if (this.settings.encryptAll) {
 			return this;
 		}
 
 		//if the paths are just equal
-		if (this.settings.foldersToEncrypt?.some(folder => folder === filePath)) {
+		if (
+			this.settings.foldersToEncrypt?.some(
+				(folder) => folder === filePath
+			)
+		) {
 			return this;
 		}
 
-		const isInAllowedFolder = this.settings.foldersToEncrypt?.some(folder => {
-			const normalizedFolder = path.resolve(folder);
-			return normalizedFilePath.startsWith(normalizedFolder + path.sep);
-		});
+		const isInAllowedFolder = this.settings.foldersToEncrypt?.some(
+			(folder) => {
+				const normalizedFolder = normalizePath(folder);
+				return normalizedFilePath.startsWith(normalizedFolder + "/");
+			}
+		);
 
 		if (!isInAllowedFolder) {
-			throw new ValidationError("FolderInSettingValidator", "This folder isn't in your path.", filePath + " folder not in path, ignoring.");
+			throw new ValidationError(
+				"FolderInSettingValidator", "This folder isn't in your path.",
+				filePath + " folder not in path, ignoring."
+			);
 		}
 
 		return this;
 	}
-
-} 
+}
